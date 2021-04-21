69f0b02ecc7944930ade980fa8a5f321e03f824e
Omar0218 <72273360+Omar0218@users.noreply.github.com>
2021-04-20 23:27:43 -0500

    Add files via upload

---
+++ Assignment02.MainClass.java
@@ -0,0 +1,25 @@
+
+package assignment02;
+
+public class MainClass {
+    
+    public static int addAllDigits(int inputNumber){
+        
+        String temp = String.valueOf(inputNumber);
+        int count = 0;
+        for(int i = 0; i < temp.length(); i++){
+            count += Integer.parseInt(temp.charAt(i)+ "");
+        }
+        
+        return count;
+        
+    }
+    
+    public static void main(String[] args){
+        
+        int result = addAllDigits(350);
+        System.out.println(result);
+        
+    }
+    
+}
